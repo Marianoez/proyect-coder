b644690d4d3a0c912c4407b1a83fbf9265356107
Marianoez <marianoezequielvara@gmail.com>
2022-06-10 08:47:43 -0300

    inicio pruebas

---
+++ js/pruebas.js
@@ -0,0 +1,18 @@
+let contacto = document.getElementById('contacto');
+contacto.remove();
+
+let contacto1 = document.getElementsByClassName('footer');
+contacto1[0].remove();
+
+let contacto2 = document.getElementById("header-prin")
+contacto2.remove();
+/* 
+let prueba = document.createElement('div');
+
+prueba.innerHTML = `<h2> AHORA TITULO NUEVO 2</h2>`;
+
+document.body.appendChild(prueba); */
+
+let prueba2 = document.getElementById('s11');
+
+prueba2.innerHTML = '<h1>La OFERTA</h1>'
