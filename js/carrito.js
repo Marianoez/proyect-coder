bc73c6242eca1978ccaed37eddfdeebabba4545a
Marianoez <marianoezequielvara@gmail.com>
2022-06-27 20:02:17 -0300

    Push al carrito con cantidad

--- js/carrito.js
+++ js/carrito.js
@@ -2,11 +2,14 @@ let cartHTML = document.getElementById('cart')
 const clearBtn = document.getElementById('clearCart')
 
 recuperar()
+showCart(cart)
 
 function recuperar() {
-    let recuperarLS = JSON.parse(localStorage.getItem('cart')) || []
+    let recuperarLS = JSON.parse(localStorage.getItem('cart'))
     recuperarLS.forEach(element => {
-        showCart(element)
+        if (element.cantidad > 0) {
+            showCart(element)
+        }
     })
     const totalBuy = recuperarLS.reduce((acc, el) => acc + el.precio, 0)
     let p = document.getElementById('total1')
@@ -16,25 +19,25 @@ function recuperar() {
 
 
 
-function showCart(addProduct) {
 
+function showCart(car) {
     let div = document.createElement('div')
     div.className = 'product';
     div.innerHTML = `<div class="card" style="width: 18rem;">
-        <img src=${addProduct.imgurl} class="card-img-top" alt="Bozal de cuero, caballo carreras, salto exhibición y Polo">
-        <!-- SEO, agregamos palabras clave caballo carreras, polo, salto  -->
-        <div class="card-body">
-            <h5 class="card-title">${addProduct.nombre}</h5>
-            <p class="card-text">${addProduct.descripcion}</p>
-        </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">Stock: ${addProduct.stock}</li>    
-            <li class="list-group-item">Precio $ ${addProduct.precio}</li>
-            <li class="list-group-item">Talle: ${addProduct.size}</li>
-            <div class="card-body">
-                <button id="btnAdd${addProduct.cod}" type="button" class="btn btn-outline-secondary">quitar</button></a>
-            </div>
-        </div>`;
+                <img src=${addProduct.imgurl} class="card-img-top" alt="Bozal de cuero, caballo carreras, salto exhibición y Polo">
+                <!-- SEO, agregamos palabras clave caballo carreras, polo, salto  -->
+                <div class="card-body">
+                    <h5 class="card-title">${addProduct.nombre}</h5>
+                    <p class="card-text">${addProduct.descripcion}</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Stock: ${addProduct.stock}</li>    
+                    <li class="list-group-item">Precio $ ${addProduct.precio}</li>
+                    <li class="list-group-item">Talle: ${addProduct.size}</li>
+                    <div class="card-body">
+                        <button id="btnAdd${addProduct.cod}" type="button" class="btn btn-outline-secondary">quitar</button></a>
+                    </div>
+                </div>`;
     cartHTML.appendChild(div)
 }
 
